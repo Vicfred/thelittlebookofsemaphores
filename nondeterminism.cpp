05c8e7bd39f5da4d049b17691800f599e69f46c3
vicfred <git@vicfred.dev>
2026-05-26 01:35:53 -0600

    initial commit, non-determinism example

---
+++ nondeterminism.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <cstdint>
+#include <iostream>
+#include <random>
+#include <thread>
+
+using namespace std;
+
+void print_char(char c) {
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<int64_t> dist(0, 5);
+
+  this_thread::sleep_for(chrono::milliseconds(dist(gen)));
+
+  cout << c;
+}
+
+int main() {
+  for (int64_t i = 0; i < 20; i++) {
+    thread a(print_char, 'a');
+    thread b(print_char, 'b');
+
+    a.join();
+    b.join();
+
+    cout << endl;
+  }
+
+  return 0;
+}
